8844692aed89453f74a90f4e246aaf473abb9420
Luis Augusto Peña Pereira <lpenap@gmail.com>
2017-10-17 09:11:48 -0400

    Improve builder to repair structures

--- src/role.builder.js
+++ src/role.builder.js
@@ -28,8 +28,14 @@ var roleBuilder = {
 	            targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
 	        }
             if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            	if (repairNeeded) {
+            		if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    	creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                	}
+            	} else {
+                	if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    	creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                	}
                 }
             }
 	    }
